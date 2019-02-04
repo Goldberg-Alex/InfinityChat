79812fa559f39eed4fb8da06b863c550bae849c5
evgeny.kuznetzov <dastar.ron@gmail.com>
2019-02-04 18:02:11 +0200

    user_list new

--- src/user_list.cpp
+++ src/user_list.cpp
@@ -12,4 +12,17 @@ void UserList::insert(std::shared_ptr<User> user)
     m_name_to_user.insert(std::pair<std::string, std::shared_ptr<User> >(user->get_name(), user));
 }
 
+void UserList::remove(int fd)
+{
+    auto user = m_fd_to_user[fd];
+    m_fd_to_user.erase(fd);
+    m_name_to_user.erase(user->get_name());
+}
+
+std::map<const int, User>::iterator UserList::begin()
+{
+    return m_fd_to_user.begin();
+}
+
+
 } // namespace ilrd
