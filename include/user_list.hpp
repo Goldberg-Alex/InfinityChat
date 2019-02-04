5b740c14a7c7dcd29cd07b8147f7019693adfdbb
evgeny.kuznetzov <dastar.ron@gmail.com>
2019-02-04 17:45:17 +0200

    user_list

--- include/user_list.hpp
+++ include/user_list.hpp
@@ -7,23 +7,26 @@
 
 #include <map>    // map
 #include <string> // string
+#include <memory> // shared_ptr
 
 #include "user.hpp"
 
 namespace ilrd {
+
 class UserList {
+public:
     explicit UserList();
 
-    void insert(User&& user_);
+    void insert(std::shared_ptr<User> user_);
     void remove(int fd);
 
     std::map<const int, User>::iterator begin();
     std::map<const int, User>::iterator end();
 
-    User& find(const std::string& name);
-    User& find(int fd);
+    std::shared_ptr<User> find(const std::string& name);
+    std::shared_ptr<User> find(int fd);
 
-    void change_name(User& user, const std::string& new_name);
+    void change_name(std::shared_ptr<User> user, const std::string& new_name);
 
     UserList(UserList&&) = default;
     UserList& operator=(UserList&&) = default;
@@ -32,8 +35,8 @@ class UserList {
     UserList& operator=(UserList&) = delete;
 
 private:
-    std::map<const int, User> m_fd_to_user;
-    std::map<std::string, User&> m_name_to_user;
+    std::map<const int, std::shared_ptr<User> > m_fd_to_user;
+    std::map<std::string, std::shared_ptr<User> > m_name_to_user;
 };
 
 } // namespace ilrd
--- src/user_list.cpp
+++ src/user_list.cpp
@@ -1,16 +1,15 @@
-#include "../include/user_list.hpp"
+#include "user_list.hpp"
 
 namespace ilrd {
 UserList::UserList() : m_fd_to_user(), m_name_to_user()
 {}
 
-void UserList::insert(User&& user_)
+void UserList::insert(std::shared_ptr<User> user)
 {
-    int fd(user_.get_fd());
-    m_fd_to_user[fd] = std::move(user_);
+    int fd(user->get_fd());
+    m_fd_to_user.insert(std::pair<int, std::shared_ptr<User> >(fd, user));
 
-    User& user = m_fd_to_user[fd];
-    m_name_to_user[user.get_name()] = user;
+    m_name_to_user.insert(std::pair<std::string, std::shared_ptr<User> >(user->get_name(), user));
 }
 
 } // namespace ilrd
