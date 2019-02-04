68ffad3d626afd40c88408e31c9af30b4529e6da
evgeny.kuznetzov <dastar.ron@gmail.com>
2019-02-04 18:28:25 +0200

    user list

--- src/user_list.cpp
+++ src/user_list.cpp
@@ -4,12 +4,12 @@ namespace ilrd {
 UserList::UserList() : m_fd_to_user(), m_name_to_user()
 {}
 
-void UserList::insert(std::shared_ptr<User> user)
+void UserList::insert(UserList::user_ptr user)
 {
     int fd(user->get_fd());
-    m_fd_to_user.insert(std::pair<int, std::shared_ptr<User> >(fd, user));
+    m_fd_to_user.insert(std::pair<int, UserList::user_ptr >(fd, user));
 
-    m_name_to_user.insert(std::pair<std::string, std::shared_ptr<User> >(user->get_name(), user));
+    m_name_to_user.insert(std::pair<std::string, UserList::user_ptr >(user->get_name(), user));
 }
 
 void UserList::remove(int fd)
@@ -19,10 +19,34 @@ void UserList::remove(int fd)
     m_name_to_user.erase(user->get_name());
 }
 
-std::map<const int, User>::iterator UserList::begin()
+std::map<const int, UserList::user_ptr >::iterator UserList::begin()
 {
     return m_fd_to_user.begin();
 }
 
+std::map<const int, UserList::user_ptr >::iterator UserList::end()
+{
+    return m_fd_to_user.end();
+}
+
+UserList::user_ptr UserList::find(const std::string& name)
+{
+    auto iter = m_name_to_user.find(name);
+    if (iter == m_name_to_user.end()) {
+        return user_ptr(nullptr);
+    }
+
+    return iter->second;
+}
+
+UserList::user_ptr UserList::find(int fd)
+{
+    auto iter = m_fd_to_user.find(fd);
+    if (iter == m_fd_to_user.end()) {
+        return user_ptr(nullptr);
+    }
+
+    return iter->second;
+}
 
 } // namespace ilrd
